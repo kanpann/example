149c5b303569cce6df5e4949a19b6691ba8cc373
gunkims <45007556+kanpann@users.noreply.github.com>
2021-07-18 16:32:05 +0900

    테스트코드 단언문 작성

--- jpa-auditing/src/test/java/dev/gunlog/domain/PostsRepositoryTests.java
+++ jpa-auditing/src/test/java/dev/gunlog/domain/PostsRepositoryTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PostsRepositoryTests {
@@ -17,10 +19,12 @@ public class PostsRepositoryTests {
     @Autowired
     private MemberRepository memberRepository;
 
+    private final String USERNAME = "gunkim";
+
     @BeforeEach
     public void createUser() {
         memberRepository.save(Member.builder()
-                .username("gunkim")
+                .username(USERNAME)
                 .password("gunkim")
                 .age(22)
                 .build());
@@ -28,13 +32,17 @@ public class PostsRepositoryTests {
 
     @Test
     @DisplayName("게시글 입력 테스트")
-    @WithMockUser(username = "gunkim")
+    @WithMockUser(username = USERNAME)
     public void saveTest() {
         postsRepository.save(Posts.builder()
                 .title("첫 게시글입니다.")
                 .contents("게시글 내용입니다.")
                 .build());
 
-        System.out.println(postsRepository.findAll().get(0));
+        Posts posts = postsRepository.findAll().get(0);
+        assertThat(posts.getTitle()).isEqualTo("첫 게시글입니다.");
+        assertThat(posts.getContents()).isEqualTo("게시글 내용입니다.");
+        assertThat(posts.getCreatedMember().getUsername()).isEqualTo(USERNAME);
+        assertThat(posts.getModifiedMember().getUsername()).isEqualTo(USERNAME);
     }
 }
